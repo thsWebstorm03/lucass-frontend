86cfa4b464ec79cc43003fe37e92e15f35941c74
thsWebstorm03 <webstorm03@gmail.com>
2023-03-22 20:37:57 +0100

    backend modify

--- src/configs/index.js
+++ src/configs/index.js
@@ -1,3 +1,3 @@
 export const PUBLIC_URL = process.env.NODE_ENV == "development" ? "http://localhost:3000/" : "https://coral-app-u2dte.ondigitalocean.app/"
 
-export const BASE_URL = process.env.NODE_ENV == "development" ? "http://localhost:5001" : "http://65.108.142.188:5000"
+export const BASE_URL = process.env.NODE_ENV == "development" ? "http://localhost:5001" : "https://shark-app-lke5e.ondigitalocean.app"
