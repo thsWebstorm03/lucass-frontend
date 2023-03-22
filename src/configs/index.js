97201d75cd19b01c97ad9d1db4ff738dbe56229c
thsWebstorm03 <webstorm03@gmail.com>
2023-03-22 19:55:19 +0100

    auth bug

--- src/configs/index.js
+++ src/configs/index.js
@@ -1,3 +1,3 @@
-export const PUBLIC_URL = process.env.NODE_ENV == "development" ? "http://localhost:3000/" : "https://plankton-app-akgi8.ondigitalocean.app/"
+export const PUBLIC_URL = process.env.NODE_ENV == "development" ? "http://localhost:3000/" : "https://coral-app-u2dte.ondigitalocean.app/"
 
 export const BASE_URL = process.env.NODE_ENV == "development" ? "http://localhost:5001" : "http://65.108.142.188:5000"
