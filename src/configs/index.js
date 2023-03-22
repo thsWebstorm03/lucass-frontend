ca230cc23ef9d76e76178211555156de2dbb441c
thsWebstorm03 <webstorm03@gmail.com>
2023-03-22 17:05:41 +0100

    building err fixed

--- src/configs/index.js
+++ src/configs/index.js
@@ -1,2 +1,3 @@
 export const PUBLIC_URL = process.env.NODE_ENV == "development" ? "http://localhost:3000/" : "https://plankton-app-akgi8.ondigitalocean.app/"
+
 export const BASE_URL = process.env.NODE_ENV == "development" ? "http://localhost:5001" : "http://65.108.142.188:5000"
