3a76a4905bf781d18f28e3cde77c5beabcf98c37
thsWebstorm03 <webstorm03@gmail.com>
2023-03-26 17:09:59 +0800

    update

--- src/configs/index.js
+++ src/configs/index.js
@@ -1,6 +1,6 @@
 export const PUBLIC_URL = process.env.NODE_ENV == "development" ? "http://localhost:3003/" : "https://coral-app-u2dte.ondigitalocean.app/"
 
-export const BASE_URL = process.env.NODE_ENV == "development" ? "http://localhost:5001" : "http://shark-app-lke5e.ondigitalocean.app"
+export const BASE_URL = process.env.NODE_ENV == "development" ? "http://localhost:5001" : "https://shark-app-lke5e.ondigitalocean.app"
 
 export const particle_options = {
    background: {
