a356a27c4795e004950cdec624d496107eaf8383
thsWebstorm03 <webstorm03@gmail.com>
2023-03-26 14:31:17 +0800

    favicon update

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -23,7 +23,7 @@ class CustomDocument extends Document {
             href='https://fonts.googleapis.com/css2?family=Public+Sans:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,500;1,600;1,700&display=swap'
           />
           <link rel='apple-touch-icon' sizes='180x180' href='/images/apple-touch-icon.png' />
-          <link rel='shortcut icon' href='/images/favicon.png' />
+          <link rel='shortcut icon' href='/images/favicon1.png' />
         </Head>
         <body>
           <Main />
